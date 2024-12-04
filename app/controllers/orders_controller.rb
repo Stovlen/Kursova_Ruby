a0daf22b285f36b3ba80f47d0efec4b97da9d737
Stovlen <stovbanolexandr@gmail.com>
2024-12-04 12:03:08 +0200

    Додано Нову Пошту

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,7 @@ class OrdersController < ApplicationController
     @cart = session[:cart] || {} # Отримуємо дані кошика із сесії
     @products = Product.where(id: @cart.keys) # Завантажуємо товари з бази за ID у кошику
     @total_price = calculate_total_price(@products, @cart) # Розраховуємо загальну суму
+    @branches = fetch_nova_poshta_branches
   end
 
   private
@@ -32,26 +33,27 @@ class OrdersController < ApplicationController
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+  # Метод для отримання відділень Нової Пошти через API
+  def fetch_nova_poshta_branches
+    api_key = 'e230aeb058a424ca3bd19ad0670ec7cd' # Вставте ваш реальний API-ключ
+    url = 'https://api.novaposhta.ua/v2.0/json/'
+    response = HTTP.post(url, json: {
+      "apiKey": api_key,
+      "modelName": "AddressGeneral",
+      "calledMethod": "getWarehouses",
+      "methodProperties": {}
+    })
+
+    data = JSON.parse(response.body.to_s)
+    data['data'].map { |branch| branch['Description'] } # Повертаємо список відділень
+  rescue StandardError => e
+    Rails.logger.error("Помилка отримання відділень: #{e.message}")
+    []
+  end
+
   # Параметри форми замовлення
   def order_params
     params.require(:order).permit(:name, :surname, :phone, :email, :delivery_option, :branch)
   end
 
-  # Метод для отримання відділень Нової Пошти через API
-  # def fetch_nova_poshta_branches
-  #   api_key = 'e230aeb058a424ca3bd19ad0670ec7cd' # Вставте ваш реальний API-ключ
-  #   url = 'https://api.novaposhta.ua/v2.0/json/'
-  #   response = HTTP.post(url, json: {
-  #     "apiKey": api_key,
-  #     "modelName": "AddressGeneral",
-  #     "calledMethod": "getWarehouses",
-  #     "methodProperties": {}
-  #   })
-  #
-  #   data = JSON.parse(response.body.to_s)
-  #   data['data'].map { |branch| branch['Description'] } # Повертаємо список відділень
-  # rescue StandardError => e
-  #   Rails.logger.error("Помилка отримання відділень: #{e.message}")
-  #   []
-  # end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ApplicationRecord
+  validates :name, :surname, :phone, :branch, presence: true
+  validates :email, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "некоректний формат" }, allow_blank: true
 end
