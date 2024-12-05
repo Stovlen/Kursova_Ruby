6b70043a81498aa89874003f822513cba1d34d86
Stovlen <stovbanolexandr@gmail.com>
2024-12-05 13:08:50 +0200

    Коректна робота успішного замовлення

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,7 +4,8 @@ class PaymentsController < ApplicationController
   end
 
   def create
-    # Простий сценарій: перенаправляємо на сторінку успіху
+    # Простий сценарій: очищуємо кошик та перенаправляємо на сторінку успіху
+    session[:cart] = {} # Очищення кошика
     redirect_to payment_success_path, notice: 'Оплата пройшла успішно!'
   end
 
